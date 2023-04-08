beb8d5aa9eb8341b35775183bc62a4c1587834ad
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2023-04-07 21:14:19 -0600

    Closes #6

--- examples/audio/audio-example.go
+++ examples/audio/audio-example.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/TannerKvarfordt/gopenai/audio"
+	"github.com/TannerKvarfordt/gopenai/authentication"
+)
+
+const OpenAITokenEnv = "OPENAI_API_KEY"
+
+func init() {
+	key := os.Getenv(OpenAITokenEnv)
+	authentication.SetAPIKey(key)
+}
+
+const (
+	model             = "whisper-1"
+	transcriptionFile = "transcription.m4a"
+	translationFile   = "translation.m4a"
+)
 
 func main() {
-	fmt.Println("Not implemented yet.")
+	{ // Transcription
+		fmt.Printf("Sending transcription request for file %s...\n", transcriptionFile)
+		r, err := audio.MakeTranscriptionRequest(&audio.TranscriptionRequest{
+			File:  transcriptionFile,
+			Model: model,
+		}, nil)
+		if err != nil {
+			fmt.Printf("Error with transcription request: %s\n", err)
+		} else {
+			fmt.Printf("Transcribed audio: %s\n", r.Text)
+		}
+	}
+
+	{ // Translation
+		fmt.Printf("Sending translation request for file %s...\n", translationFile)
+		r, err := audio.MakeTranslationRequest(&audio.TranslationRequest{
+			File:  translationFile,
+			Model: model,
+		}, nil)
+		if err != nil {
+			fmt.Printf("Error with translation request: %s\n", err)
+		} else {
+			fmt.Printf("Translated audio: %s\n", r.Text)
+		}
+	}
 }
