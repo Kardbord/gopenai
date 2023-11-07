f889f004661a86065bb3448e8d6551ec133bfc1b
Tanner Kvarfordt <tanner.kvarfordt@gmail.com>
2023-11-06 22:01:11 -0700

    Updated images.CreationRequest

--- images/images.go
+++ images/images.go
@@ -25,14 +25,40 @@ const (
 )
 
 const (
-	SmallImage  string = "256x256"
-	MediumImage string = "512x512"
-	LargeImage  string = "1024x1024"
+	Dalle2SmallImage  = "256x256"
+	Dalle2MediumImage = "512x512"
+	Dalle2LargeImage  = "1024x1024"
+
+	Dalle3SquareImage    = "1024x1024"
+	Dalle3LandscapeImage = "1792x1024"
+	Dalle3PortraitImage  = "1024x1792"
+
+	// Deprecated: Use Dalle2SmallImage instead.
+	SmallImage = Dalle2SmallImage
+	// Deprecated: Use Dalle2MediumImage instead.
+	MediumImage = Dalle2MediumImage
+	// Deprecated: Use Dalle2LargeImage instead.
+	LargeImage = Dalle2LargeImage
 )
 
 const (
 	ResponseFormatURL     = "url"
 	ResponseFormatB64JSON = "b64_json"
+)
+
+const (
+	ModelDalle2 = "dall-e-2"
+	ModelDalle3 = "dall-e-3"
+)
+
+const (
+	QualityStandard = "standard"
+	QualityHD       = "hd"
+)
+
+const (
+	StyleVivid   = "vivid"
+	StyleNatural = "natural"
 )
 
 // Response structure for the image API endpoint.
@@ -47,17 +73,35 @@ type Response struct {
 
 // Request structure for the image creation API endpoint.
 type CreationRequest struct {
-	// A text description of the desired image(s). The maximum length is 1000 characters.
+	// A text description of the desired image(s).
+	// The maximum length is 1000 characters for dall-e-2 and 4000 characters for dall-e-3.
 	Prompt string `json:"prompt,omitempty"`
 
+	// The model to use for image generation.
+	Model string `json:"model,omitempty"`
+
 	// The number of images to generate. Must be between 1 and 10.
+	// For dall-e-3, only n=1 is supported.
 	N *uint64 `json:"n,omitempty"`
 
-	// The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024.
-	Size string `json:"size,omitempty"`
+	// The quality of the image that will be generated.
+	// "hd" creates images with finer details and greater consistency across the image.
+	// This param is only supported for dall-e-3.
+	Quality string `json:"quality,omitempty"`
 
 	// The format in which the generated images are returned. Must be one of url or b64_json.
 	ResponseFormat string `json:"response_format,omitempty"`
+
+	// The size of the generated images.
+	// Must be one of 256x256, 512x512, or 1024x1024 for dall-e-2.
+	// Must be one of 1024x1024, 1792x1024, or 1024x1792 for dall-e-3 models.
+	Size string `json:"size,omitempty"`
+
+	// The style of the generated images. Must be one of vivid or natural.
+	// Vivid causes the model to lean towards generating hyper-real and dramatic images.
+	// Natural causes the model to produce more natural, less hyper-real looking images.
+	// This param is only supported for dall-e-3.
+	Style string `json:"style,omitempty"`
 
 	// A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse.
 	User string `json:"user,omitempty"`
